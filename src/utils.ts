07f0fbbaf8716f8fa650af92ba5c4860b1063cac
lilezek <devezek@gmail.com>
2024-03-09 15:20:45 +0100

    feat: added support for both sha256 implementations in web (window.crypto) and node.

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,16 @@
-import * as crypto from "crypto";
+let nodeCrypto = import("crypto");
+let webCrypto = typeof window !== "undefined" && window.crypto;
 
-export function hashString(input: string) {
-    return crypto.createHash("sha256").update(input).digest("hex");
+export async function hashString(input: string) {
+    if (webCrypto) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(input);
+        return webCrypto.subtle.digest("SHA-256", data).then(hashBuffer => {
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+            return hashHex;
+        });
+    }
+
+    return (await nodeCrypto).createHash("sha256").update(input).digest("hex");
 }
